ab291f71d722f7a646b003641ca2d153ba179624
jamfly <cocoredju222@gmail.com>
2020-08-22 21:33:03 +0800

    Add main script to train
    Signed-off-by: jamfly <cocoredju222@gmail.com>

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
+from data_loader.data_loader import DataLoader
 from model.transformer import Transformer
+from constants.constants import Constants
+from train import Trainer
 
+import torch.optim as optim
+import numpy as np
 import torch
+
+import random
 
 if __name__ == '__main__':
   device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+  
+  const = Constants()
+  data_loader = DataLoader(device=device)
+  
+  SOURCE_VOCAB_SIZE = data_loader.get_source_vocab_size()
+  TARGET_VOCAB_SIZE = data_loader.get_target_vocab_size()
+  SRC_PAD_IDX = data_loader.get_source_padding_index()
+  TRG_PAD_IDX = data_loader.get_target_padding_index()
 
-  source = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0], [1, 8, 7, 3, 4, 5, 6, 7, 2]]).to(device)
-  target = torch.tensor([[1, 7, 4, 3, 5, 9, 2, 0], [1, 5, 6, 2, 4, 7, 6, 2]]).to(device)
+  train_iterator, valid_iterator, test_iterator = data_loader.get_iterator()
 
-  source_padding_index = 0
-  target_padding_index = 0
-  source_vocab_size = 10
-  target_vocab_size = 10
+  random.seed(const.SEED)
+  np.random.seed(const.SEED)
+  torch.manual_seed(const.SEED)
+  torch.cuda.manual_seed(const.SEED)
+  torch.backends.cudnn.deterministic = True
 
   model = Transformer(
-    source_vocab_size=source_vocab_size,
-    target_vocab_size=target_vocab_size,
-    source_padding_index=source_padding_index,
-    target_padding_index=target_padding_index,
+    source_vocab_size=SOURCE_VOCAB_SIZE,
+    target_vocab_size=TARGET_VOCAB_SIZE,
+    source_padding_index=SRC_PAD_IDX,
+    target_padding_index=TRG_PAD_IDX,
     device=device
   ).to(device)
 
-  out = model(source=source, target=target[:, :-1])
-  
-  print(out)
-  print(out.shape)  
+  model.apply(model_utils.initialize_weights)
+
+  optimizer = torch.optim.Adam(model.parameters(), lr=const.LEARNING_RATE)
+
+  trainer = Trainer(
+    const=const,
+    model=model,
+    optimizer=optimizer,
+    train_iterator=train_iterator,
+    valid_iterator=valid_iterator,
+    device=device
+  )
+
+  trainer.train()
