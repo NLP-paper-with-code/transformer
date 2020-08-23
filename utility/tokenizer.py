be6a97a7e5ed9dd3f04fbbc07a5839cd8e806b24
jamfly <cocoredju222@gmail.com>
2020-08-23 15:58:01 +0800

    Fix tokenizor issue
    Signed-off-by: jamfly <cocoredju222@gmail.com>

--- utility/tokenizer.py
+++ utility/tokenizer.py
@@ -3,17 +3,18 @@ from typing import List
 import de_core_news_md
 import en_core_web_md
 
+spacy_de = de_core_news_md.load()
+spacy_en = en_core_web_md.load()
+
 def tokenize_de(text: str) -> List[str]:
   '''
   Tokenize German text from a string into a list of strings
   '''
-  spacy_de = de_core_news_md.load()
   return [token.text for token in spacy_de.tokenizer(text)]
 
 def tokenize_en(text: str) -> List[str]:
   '''
   Tokenize English text from a string into a list of strings
   '''
-  spacy_en = en_core_web_md.load()
   return [token.text for token in spacy_en.tokenizer(text)]
   
